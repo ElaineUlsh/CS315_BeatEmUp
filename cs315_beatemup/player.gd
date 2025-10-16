79240cfcd4ebfbff9edf2bd86f49ca2fd2349e4d
Elaine Ulsh <elaineulsh1126@gmail.com>
2025-10-16 13:30:27 -0400

    Add animations

--- cs315_beatemup/player.gd
+++ cs315_beatemup/player.gd
@@ -2,13 +2,22 @@ extends CharacterBody2D
 
 @export var speed = 100
 
+@onready var anim_tree = $AnimationTree
+
 func _process(delta: float) -> void:
 	var dir = Input.get_vector("left", "right", "up", "down")
 	
 	self.velocity = dir * speed
 	
+	if velocity.length() > 0.01:
+		anim_tree['parameters/conditions/walk'] = true
+		anim_tree['parameters/conditions/idle'] = false
+	else:
+		anim_tree['parameters/conditions/walk'] = false
+		anim_tree['parameters/conditions/idle'] = true
+	
 	move_and_slide()
 	
 	pass
 
-## stopped video at 11:51
+## stopped video at 26:50
